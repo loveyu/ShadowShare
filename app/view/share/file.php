70c44a819f3d8b647986a3ccadf02733a80df85f
loveyu <admin@loveyu.info>
2015-02-18 22:07:57 +0800

    更新文件分享文案

--- app/view/share/file.php
+++ app/view/share/file.php
@@ -9,7 +9,7 @@ $this->get_header("文件分享");
 		<h3>文件分享:</h3>
 		<p>文件名：<?php echo $__share->getFileName()?></p>
 		<p>文件大小：<?php echo $__share->getFileReadSize()?></p>
-		<p>下载地址:：<a href="<?php echo get_url($__share->getUname())?>?m=bin"><?php echo get_url($__share->getUname())?>?m=bin</a></p>
+		<p>下载地址：<a href="<?php echo get_url($__share->getUname())?>?m=bin"><?php echo get_url($__share->getUname())?>?m=bin</a></p>
 	</div>
 <?php
 $this->get_footer();
